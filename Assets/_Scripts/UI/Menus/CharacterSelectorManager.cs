5d6fc23b65b052d5391b3be60c461f8d2454a765
Alejo <alejoczombos@gmail.com>
2023-03-28 16:59:22 -0300

    Selección de Personaje y corrección de Bugs

---
+++ Assets/_Scripts/UI/Menus/CharacterSelectorManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelectorManager : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private ScenesManager scenesManager;
+    [SerializeField] private Image BigImage;
+    [SerializeField] private Image smallImage;
+
+    private float timeSinceLastClick;
+    private float timeSinceClicks = 0.3f;
+    private int currentSelectedCharacter;
+
+    private void Start()
+    {
+        animator.SetInteger("SelCharacter", 1);
+    }
+
+    private void Update()
+    {
+        timeSinceLastClick += Time.deltaTime;
+        if (timeSinceLastClick < timeSinceClicks)
+        {
+            return;
+        }
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            timeSinceLastClick = 0.0f;
+            currentSelectedCharacter = currentSelectedCharacter + 1;
+        }else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            timeSinceLastClick = 0.0f;
+            currentSelectedCharacter = currentSelectedCharacter - 1;
+        }
+        currentSelectedCharacter = Mathf.Clamp(currentSelectedCharacter, 0, 2);
+        animator.SetInteger("SelCharacter", currentSelectedCharacter);
+        if (Input.GetAxisRaw("Submit") != 0)
+        {
+            if (currentSelectedCharacter == 1)
+            {
+                scenesManager.ChangeSceneToGame();
+            }
+        }
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("CharacterSelector_Juana") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+        {
+            smallImage.color = new Color(1, 1, 1, 0);
+            BigImage.color = new Color(1, 1, 1, 1);
+        }
+    }
+}
