a4d71fce82c2a8e2421ae926d032f3e60071dcf6
AgustinCampanella <campanellagustin@hotmail.com>
2023-04-01 21:17:18 -0300

    + Animaciones

--- Assets/_Scripts/PlayerAttack.cs
+++ Assets/_Scripts/PlayerAttack.cs
@@ -32,7 +32,6 @@ public class PlayerAttack : MonoBehaviour
         currentHealth = maxHealth;
         transformar = gameObject.GetComponentInParent<Transform>();
         animator = gameObject.GetComponent<Animator>();
-        animator.SetInteger("combo", -1);
     }
 
     // Update is called once per frame
@@ -51,48 +50,21 @@ public class PlayerAttack : MonoBehaviour
         {
             contador = false;
             animator.SetBool("noAttack", true);
-            animator.SetInteger("combo", -1);
             x = 0;
         }
 
 
-        if (Input.GetButtonDown("Attack") && animator.GetInteger("combo") == -1)
+        if (Input.GetButtonDown("Attack"))
         {
-            Attack();
+            
             contador = true;
             animator.SetTrigger("Attack");
             animator.SetBool("noAttack", false);
-            animator.SetInteger("combo", 0);
             x = 0;
 
         }
 
-        if (Input.GetButtonDown("Attack") && x > 0.15f && animator.GetInteger("combo") == 0)
-        {
-            Attack();
-            contador = true;
-            animator.SetBool("noAttack", false);
-            animator.SetInteger("combo", 1);
-            x = 0;
-        }
-
-        if (Input.GetButtonDown("Attack") && x > 0.15f && animator.GetInteger("combo") == 1)
-        {
-            Attack();
-            contador = true;
-            animator.SetBool("noAttack", false);
-            animator.SetInteger("combo", 2);
-            x = 0;
-        }
-
-        if (Input.GetButtonDown("Attack") && x > 0.15f && animator.GetInteger("combo") == 2)
-        {
-            Attack();
-            contador = true;
-            animator.SetBool("noAttack", false);
-            animator.SetInteger("combo", 0);
-            x = 0;
-        }
+        
     }
 
     public void TakeDamage(int damage)
